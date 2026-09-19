dafe5e4a4075610c973cb955fcb34d30ad97ee9f
carlospmbv-ctrl <carlospmbv@gmail.com>
2026-09-19 18:32:43 -0500

    Actualizar versión

--- IMG_Classifier/streamlit_app.py
+++ IMG_Classifier/streamlit_app.py
@@ -14,8 +14,8 @@ def load_controller():
 model_controller = load_controller()
 preprocessor = DataPreprocessing()
 
-st.title("🌱 Clasificador de Opiniones Ciudadanas según ODS (Agenda 2030)")
-st.write("Ingresa un texto o propuesta ciudadana para identificar su relación semántica con los ODS [1, 2].")
+st.title("🌱 Clasificador de Propuestas en línea con los ODS (Agenda 2030)")
+st.write("Ingresa un texto o propuesta para identificar su relación semántica con los Objetivos de Desarrollo Sostenible (ODS).")
 
 user_text = st.text_area("Ingresa la propuesta ciudadana:", height=150)
 
